4839f4ff1e6bebf8209c568097af20a13f5f8956
Timmy Xiao <timmyxiao1@gmail.com>
2019-12-22 19:43:13 -0500

    Use parent pointers

--- solver.py
+++ solver.py
@@ -1,11 +1,13 @@
 import api
 from collections import deque
 
+
 def solve():
     random_page = api.get_random_page()
     hitler_page_id = str(api.title_to_id('Adolf_Hitler'))
     visited = {random_page}
     queue = deque([random_page])
+    parent = {random_page: None}
 
     while queue:
         page_id = queue.popleft()
@@ -13,6 +15,10 @@ def solve():
         print(page_id)
         if page_id == hitler_page_id:
             print("YOU FOUND HITLER!")
+            res = page_id
+            while res:
+                print('p', page_id)
+                res = parent[res]
             return
         try:
             neighbors = api.get_links_from_page(page_id)
@@ -20,6 +26,7 @@ def solve():
             neighbors = []
         for neighbor in neighbors:
             if neighbor not in visited:
+                parent[neighbor] = page_id
                 visited.add(neighbor)
                 queue.append(neighbor)
 
